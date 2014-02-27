4b8c1f7a665511ca41b14677178fc8a6e958f868
sgowris2 <sgowris2@gmail.com>
2014-02-27 06:35:21 +0530

    Updated graph labels to True and Estimated

--- CurrentVersion/output/plot/plotPolicy.m
+++ CurrentVersion/output/plot/plotPolicy.m
@@ -8,19 +8,19 @@ plot(DataSet{policyIndex},mod(1:size(DataSet{policyIndex}),-noOfPhasesInACycle)+
 hold on;
 plot(agentDataSet{policyIndex}, mod(1:size(DataSet{policyIndex}),-noOfPhasesInACycle)+8,'sr-')
 
-title('Observed vs. Simulated policy');
+title('True vs. Estimated policy');
 xlabel('Time(s)');
 ylabel('Phase #');
-legend('Observed phase switches','Simulated phase switches');
+legend('True phase switches','Estimated phase switches');
 
 figure;
 plot(DataSet{policyIndex},1:size(DataSet{policyIndex}) ,'o')
 hold on;
 plot(agentDataSet{policyIndex}, 1:size(DataSet{policyIndex}),'sr')
-title('Observed vs. Simulated policy');
+title('True vs. Estimated policy');
 xlabel('Time(s)');
 ylabel('Phase switch #');
-legend('Observed phase switches','Simulated phase switches');
+legend('True phase switches','Estimated phase switches');
 
 figure;
 A = DataSet{policyIndex}(2:end) - DataSet{policyIndex}(1:end-1);
@@ -29,7 +29,7 @@ B = (agentDataSet{policyIndex}(2:end) - agentDataSet{policyIndex}(1:end-1));
 B = [agentDataSet{policyIndex}(1);B];
 A = [A,B];
 bar(A,'grouped');
-title('Observed vs. Simulated phase lengths');
+title('True vs. Estimated phase lengths');
 xlabel('Phase #');
 ylabel('Phase length (s)');
-legend('Observed phase lengths', 'Simulated phase lengths');
+legend('True phase lengths', 'Estimated phase lengths');
